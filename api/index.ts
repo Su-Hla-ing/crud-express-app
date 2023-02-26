04d4bdd27c2d0efef7e8c2ce9a252551af18d3fb
Su Hlaing <suhlaing200341@gmail.com>
2023-02-26 19:38:59 +0630

    first fixed

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
 
 dotenv.config();
 const Api_url  = process.env.ApiUrl;
@@ -7,10 +8,11 @@ console.log(Api_url );
 
 const app = express();
 app.use(express.static('public'));
+app.use(express.json())
 
 const users = [
-    {name: 'user1', email: 'user1@gmail.com'},
-    {name: 'user2', email: 'user2@gmail.com'}
+    {name: 'user1', email: 'user1@gmail.com', id: "1"},
+    {name: 'user2', email: 'user2@gmail.com', id: "2"}
 ]
 
 
@@ -46,17 +48,40 @@ app.get('/api/users', (req:Request, res:Response) => {
 
 
 app.post('/api/users', (req:Request, res:Response) => {
-    let data= '';
-    req.on("data", (chunk) => {
-        data += chunk;
-    });
-    req.on("end", () => {
-        const newUserDatas = JSON.parse(data);
-        users.push(newUserDatas)
-       
-    })
+    const dataFromScript =req.body;
+    users.push(dataFromScript)
     res.send(users)
 });
+
+app.put('/api/users', (req: Request, res: Response) => {
+    const userReqid = req.body.email;
+    // console.log(userReqid)
+    const isSameId = users.find(user => user.email === userReqid)
+    // console.log("hello")
+    // console.log(isSameId)
+    if(isSameId){
+        isSameId.name = req.body.name;
+        res.send(users);
+    }
+    
+});
+
+app.delete('/api/users', (req:Request, res:Response) => {
+   
+    const isEmail = users.find(user => user.email === req.body.email)
+    console.log(isEmail)
+    if(isEmail){
+        const indexUser = users.indexOf(isEmail);
+        console.log(indexUser)
+        users.splice(indexUser, 1);
+        
+        res.setHeader('Content-Type', 'application/json');
+        res.setHeader('X-Powered-By', 'Express');
+        res.send(users)
+    }else{
+        res.send("<h1>Pages are not found</h1>")
+    }
+})
 
 app.listen(3000, () => {
     console.log("Server started: listening on port 3000")
--- public/script.js
+++ public/script.js
@@ -3,22 +3,26 @@ const formContainer = document.querySelector('.form_container');
 const userName = document.querySelector(".user_name");
 const userEmail = document.querySelector(".user_email");
 const button = document.querySelector("button");
+const updateUsername = document.getElementById("updateUsername");
+const updateUserEmail = document.getElementById("updateUserEmail");
+const errmessage = document.querySelectorAll(".message");
 
 const api_Url = localStorage.getItem("ApiUrl");
  console.log(api_Url)
-let data;
+let userdata;
 
 // Create User container
 const createUser = () => {
-    for (let i = 0; i < data.length; i++) {
+    for (let i = 0; i < userdata.length; i++) {
         usersContainer.innerHTML += `
         <div class="user">
-            <h2>${data[i].name}</h2>
-            <button type="button"  class="btn btn-danger " >Update</button>
-            <button type="button"  class="btn btn-info " >Delete</button>
+            <h2>${userdata[i].name}</h2>
+            <button type="button"  data-bs-toggle="modal" data-bs-target="#exampleModal"  class="btn btn-danger " >Update</button>
+            <button type="button" onclick="deleteUser()" class="btn btn-info " >Delete</button>
 
         </div>
         `  
+        
     }
 }
 
@@ -33,8 +37,8 @@ const getData = async () => {
    
     if(api_Url){
         const responseData = await fetch(`${api_Url}/users`)
-        data = await responseData.json();
-        console.log(data);
+        userdata = await responseData.json();
+        console.log(userdata);
         createUser();
     }else{
         window.location.href = "/api"
@@ -45,20 +49,61 @@ getData();
 
 
 // Post method
+let idNumbers = 2;
 const formSubmit = () => {
+    
+    if (userName.value === '' || userEmail.value === '') {
+       
+       errmessage.forEach((errmessage) => {
+         errmessage.classList.add("color-red");
+         errmessage.textContent = "Please fill out this field."
+       })
+       return
+    }
     const postData = async () => {
         const response = await fetch(`${api_Url}/users`, {
             method: "POST",
-            body: JSON.stringify({"name": userName.value, "email": userEmail.value})
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify({"name": userName.value, "email": userEmail.value, "id": `${idNumbers += 1}`})
         })
     }
-  
-   
     postData();
     createUser();
     getData();
     inputValueLess();
+        
+   
+      
+   
+    
+    
+};
+
+// Put Method
+const update = async () => {
+   
+   const response = await fetch (`${api_Url}/users`, {
+    method: "PUT",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({"name": updateUsername.value, "email": updateUserEmail.value, "id": `${idNumbers += 1}`})
+   });
+   createUser();
+    getData();
+    usersContainer.innerHTML = '';
+    updateUserEmail.value = '';
+    updateUsername.value = '';
+};
+
+// Delete Method
+const deleteUser = async () => {
+    console.log("hello frontend")
+    const response = await fetch (`${api_Url}/users`, {
+        method: "DELETE",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(...userdata)
+    });
+    console.log(...userdata)
+    usersContainer.innerHTML = '';
+    getData();
 }
-
-
 
